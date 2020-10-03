90a455d9abdd4a60494d71f691d6281babf842f7
Benjamin Frye <ben.j.frye@gmail.com>
2020-10-03 10:58:37 +1000

    Add player movement.

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerMovementController : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() 
+    {
+        Vector3 pos = transform.position;
+        pos.x += Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (Input.GetAxis("Horizontal") > 0.0f)
+            spriteRenderer.flipX = true;
+        else if (Input.GetAxis("Horizontal") < 0.0f)
+            spriteRenderer.flipX = false;
+    }
+}
