bf0bdb8649ac06d504f636cd04ddc03707e2149d
Benjamin Frye <ben.j.frye@gmail.com>
2020-10-03 15:56:43 +1000

    Add player spawn.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,21 +6,28 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance {get; private set; }
 
+    private Transform player;
+    private Vector3 spawnPoint;
+
     void Awake()
     {
         Debug.Assert(instance == null);
         instance = this;
+        player = GameObject.FindWithTag("Player").transform;
+        spawnPoint = player.position;
     }
 
     // Go to the Next Stage
     public void NextStage()
     {
         Debug.Log("Next");
+        player.position = spawnPoint;
     }
 
     // Go to the Previous Stage
     public void PreviousStage()
     {
         Debug.Log("Previous");
+        player.position = spawnPoint;
     }
 }
